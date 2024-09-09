df203ee5cf700044320a11f6605e10462ccdeb69
Paul Vilca <pvilca@mpdpjujuy.gob.ar>
2024-09-09 19:04:23 -0300

    update whitenoise

--- proyecto_uno/urls.py
+++ proyecto_uno/urls.py
@@ -2,7 +2,7 @@
 URL configuration for proyecto_uno project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from apps.publico import views as publicoViews
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    #--------VIEW
+    path('', publicoViews.IndexView.as_view(), name='index'),
 ]
