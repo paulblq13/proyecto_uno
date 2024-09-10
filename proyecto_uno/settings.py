8813bbbe12fdb06ff56142290c4234fd076a05cb
PaulBlq13 <paulevilca@gmail.com>
2024-09-10 01:20:39 -0300

    update settings

--- proyecto_uno/settings.py
+++ proyecto_uno/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$u_4i50mkj^^=+2ecrzyal4^m&3)#4dssr-^xv&trhw@ka$tb1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
