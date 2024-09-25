648dbeb3f7fae35a394fbca67dd62372439c79f3
Paul Vilca <pvilca@mpdpjujuy.gob.ar>
2024-09-25 20:58:21 -0300

    UPDATE lista noticias

--- apps/prueba/admin.py
+++ apps/prueba/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
 # Register your models here.
+from apps.noticias.models import Noticia
+
+class noticiaAdmin(admin.ModelAdmin):
+    list_display = ('id', 'titulo')
+    search_fields = ('titulo', )
+
+admin.site.register(Noticia, noticiaAdmin)
