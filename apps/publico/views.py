44234b40bb34aa6bd66acb3e451dbc6b78e6b028
Paul Vilca <pvilca@mpdpjujuy.gob.ar>
2024-10-23 21:01:49 -0300

    UPDATE INVITACION 03 flechas animadas

--- apps/publico/views.py
+++ apps/publico/views.py
@@ -2,6 +2,10 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.urls import reverse
+#==FOTOS TIEMPO REAL==
+from django.shortcuts import render
+from django.core.files.storage import FileSystemStorage
+#==FIN FOTOS TIEMPO REAL==
 
 #-----------------------VIEWS-------------------------
 from django.views.generic import TemplateView
@@ -21,3 +25,14 @@ class Invitacion03View(TemplateView):
 class EscritorioView(TemplateView):
     template_name = 'publico/escritorio.html'
 
+def upload_photo(request):
+    if request.method == 'POST' and request.FILES['photo']:
+        photo = request.FILES['photo']
+        fs = FileSystemStorage()
+        filename = fs.save(photo.name, photo)
+        return render(request, 'upload_success.html', {'filename': filename})
+    return render(request, 'upload_photo.html')
+
+def gallery(request):
+    photos = FileSystemStorage().listdir('')[1]  # Listar todas las fotos
+    return render(request, 'gallery.html', {'photos': photos})
