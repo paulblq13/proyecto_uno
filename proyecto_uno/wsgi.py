356a25f93a5df7b47b4817b8d16f35fdef028d4c
Paul Vilca <pvilca@mpdpjujuy.gob.ar>
2024-11-07 19:40:24 -0300

    update wsgi

--- proyecto_uno/wsgi.py
+++ proyecto_uno/wsgi.py
@@ -8,6 +8,9 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
+#==HEROKU STATICS==
+from dj_static import Cling
+#==HEROKU STATICS==
 from django.core.wsgi import get_wsgi_application
 from whitenoise import WhiteNoise
 from proyecto_uno.settings import BASE_DIR
@@ -15,5 +18,6 @@ from proyecto_uno.settings import BASE_DIR
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proyecto_uno.settings')
 
 application = get_wsgi_application()
-application = WhiteNoise(application, root=os.path.join(BASE_DIR, 'staticfiles'))
+#application = WhiteNoise(application, root=os.path.join(BASE_DIR, 'staticfiles'))
+application = Cling(get_wsgi_application())
 
