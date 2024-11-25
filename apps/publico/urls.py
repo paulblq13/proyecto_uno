e889e0ebf1b9e024c75e28c155629ad2fd59fb98
PaulBlq13 <paulevilca@gmail.com>
2024-11-25 11:51:09 -0300

    update invitacion01 toro, mp3, url

--- apps/publico/urls.py
+++ apps/publico/urls.py
@@ -5,7 +5,7 @@ from apps.publico import views as publicoViews
 urlpatterns = [
     #DEF
     #VIEW
-    path('invitacion01/', publicoViews.Invitacion01View.as_view(), name='invitacion01'),        
+    path('serexv1/', publicoViews.Invitacion01View.as_view(), name='invitacion01'),        
     path('invitacion02/', publicoViews.Invitacion02View.as_view(), name='invitacion02'), 
     path('invitacion03/', publicoViews.Invitacion03View.as_view(), name='invitacion03'),       
     path('subirfoto/', views.upload_photo, name='subirfoto'),
