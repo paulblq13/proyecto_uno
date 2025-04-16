af550623b6504218812d22e08e833022d24a9352
Paul Vilca <pvilca@mpdpjujuy.gob.ar>
2025-04-16 20:02:18 -0300

    Update MODELO todoventas

--- apps/todoventas/admin.py
+++ apps/todoventas/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from apps.todoventas.models import Categorias, Productos
+
+#Categorias
+class categoriasAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nombre',)
+    search_fields = ('id', 'nombre',)
+
+admin.site.register(Categorias, categoriasAdmin)
+#Productos
+class productosAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nombre',)
+    search_fields = ('id', 'nombre',)
+
+admin.site.register(Productos, productosAdmin)
--- apps/todoventas/models.py
+++ apps/todoventas/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Categorias(models.Model):
+    nombre = models.CharField(max_length=50, null=True, blank=True)
+    observacion = models.CharField(max_length=300, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.nombre}"
+    
+class Productos(models.Model):
+    nombre = models.CharField(max_length=150, null=True, blank=True)
+    detalle = models.CharField(max_length=1000, null=True, blank=True)
+    estado = models.CharField(max_length=1000, null=True, blank=True)
+    observacion = models.CharField(max_length=300, null=True, blank=True)
+    precio = models.DecimalField(max_digits=10, decimal_places=2, default=0.0)
+    actualizado = models.DateTimeField()
+    serial = models.CharField(max_length=100, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.nombre}"    
